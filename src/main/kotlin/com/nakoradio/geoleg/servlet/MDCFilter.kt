9a2c6ff5a18378d274cbfca4301c2b6f1abe07cb
Arch-vile <mikko.ravimo@gmail.com>
2020-09-01 19:49:30 +0300

    Setup MDC context for logging

---
+++ src/main/kotlin/com/nakoradio/geoleg/servlet/MDCFilter.kt
@@ -0,0 +1,34 @@
+package com.nakoradio.geoleg.servlet
+
+import com.nakoradio.geoleg.controllers.COOKIE_NAME
+import com.nakoradio.geoleg.model.StateCookie
+import com.nakoradio.geoleg.services.CookieManager
+import org.slf4j.MDC
+import org.springframework.stereotype.Component
+import javax.servlet.Filter
+import javax.servlet.FilterChain
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+import javax.servlet.http.HttpFilter
+import javax.servlet.http.HttpServletRequest
+
+@Component
+class MDCFilter(val cookieManager: CookieManager) : Filter {
+
+    override fun doFilter(request: ServletRequest?, response: ServletResponse?, next: FilterChain?) {
+        val httpRequest = request as HttpServletRequest
+
+        val cookieData = httpRequest.cookies.find { it.name == COOKIE_NAME }?.value
+        val cookie = cookieData?.let {
+        cookieManager.fromWebCookie(cookieData)
+        }
+
+        val context = LogContext(httpRequest.requestURI, cookie)
+
+        MDC.put("logContext", context.toString())
+        next!!.doFilter(request,response)
+    }
+
+}
+
+data class LogContext(val URI: String, val cookie: StateCookie?)
